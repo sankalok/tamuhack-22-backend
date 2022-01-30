696d51469dfa3790b4f294fbadf20e48c318010a
Sankalok Sen <ssen2001@connect.hku.hk>
2022-01-30 12:50:22 +0800

    Added getUserData endpoint

--- source/api/controllers/getUserData.ts
+++ source/api/controllers/getUserData.ts
@@ -6,7 +6,7 @@ export default async (req: Request, res: Response) => {
 
     const body = req.body
 
-    const user = await User.find({user_id : body.user_id});
+    const user = await User.findOne({user_id : body.user_id});
 
     return res.status(200).json(user);
 }
